db4d65ec14a30ffc20817fffeaf9b16d57684eab
Arseni Bobryshau <st72493@upce.cz>
2025-12-26 17:38:32 +0100

    Add Spring Boot skeleton

---
+++ backend/src/main/kotlin/com/domluveno/DomluvenoApplication.kt
@@ -0,0 +1,12 @@
+package com.domluveno
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DomluvenoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DomluvenoApplication>(*args)
+}
+
